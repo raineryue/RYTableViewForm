4190dc0dee5a6f16b9ab9e53d7ed793958ff5a59
Rainer <it_jinlei.yue@163.com>
2017-12-03 18:11:08 +0800

    表格试图表单
    表格试图表单初始化

--- RYTableViewForm/ViewController.m
+++ RYTableViewForm/ViewController.m
@@ -10,20 +10,51 @@
 
 @interface ViewController ()
 
+@property (nonatomic, strong) NSMutableArray *dataArray;
+
 @end
 
 @implementation ViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
 }
 
-
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
+    return 1;
 }
 
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return self.dataArray.count;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    static NSString *tableViewCellIdentifier = @"RYTableViewCellIdentifier";
+    
+    UITableViewCell *tableViewCell = [tableView dequeueReusableCellWithIdentifier:tableViewCellIdentifier];
+    
+    if (nil == tableViewCell) {
+        tableViewCell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:tableViewCellIdentifier];
+    }
+    
+    NSString *titleString = self.dataArray[indexPath.row];
+    
+    tableViewCell.textLabel.text = titleString;
+    
+    return tableViewCell;
+}
+
+- (NSMutableArray *)dataArray {
+    if (nil == _dataArray) {
+        _dataArray = [NSMutableArray array];
+        
+        [_dataArray addObject:@"文本框控件（UITextField）"];
+        [_dataArray addObject:@"开关控件  （UISwitch）"];
+        [_dataArray addObject:@"带标签图片的表格"];
+    }
+    
+    return _dataArray;
+}
 
 @end
